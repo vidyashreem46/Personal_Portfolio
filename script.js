88677b22bca533071c45fd6606417590023f4e63
vidyashreem46 <vidyashreem46@gmail.com>
2026-08-31 21:10:50 +0530

    Add new portfolio website design

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>{document.querySelectorAll('nav a').forEach(x=>x.classList.remove('active'));a.classList.add('active')}));
